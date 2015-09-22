1fb1acea399d82afc8cec899a04dd77051710918
Sven Noreillie <sven.noreillie@digipolis.be>
2015-09-22 14:34:47 +0200

    Release v0.9.2

--- dist/digipolis-codetabel-dropdown.js
+++ dist/digipolis-codetabel-dropdown.js
@@ -97,40 +97,4 @@
         };
     }]);
  
-})();;'use strict';
-(function (module) {
-  try {
-    module = angular.module('digipolis.codetabelservice');
-  } catch (e) {
-    module = angular.module('digipolis.codetabelservice', []);
-  }
-  module.factory('digipolisCodetabelService', ['AppService', 'awelzijnHelperHttp', 'AppConfig', function (appService, helper, appConfig) {
-        
-        function _get(codeTabelApiRoute) {
-            var options = {
-                authenticate: true,
-                transform: function(response) {
-                    var elements = [];
-
-                    for (var key in response) {
-                        if (key.indexOf('listOf', 0) === 0)
-                        {
-                            elements = response[key];
-                            break;
-                        }
-                    }
-
-                    return elements;
-                }
-            };
-
-            return helper.get(appConfig.apiRoot + codeTabelApiRoute + '/', options);
-        }
-
-        appService.logger.creation('digipolisCodetabelService');
-
-        return {
-            get: _get
-        };
-    }]);
 })();;
